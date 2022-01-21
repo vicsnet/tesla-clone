fcf95d5f05718cf16de0870e56d0a78962ede633
vicsnet <adesanmiadebisi@gmail.com>
2022-01-21 03:09:10 -0800

    ADD: Sections for the TESLA

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Header() {
     return (
-        
-        <div>
-            Header
-      </div>
+        <Container>
+            <a href="#">
+                <img src="/images/logo.svg" alt="" />
+            </a>
+            <Menu>
+
+            </Menu>
+        </Container>
     );
 }
 
 export default Header;
+const Container = styled.div`
+
+
+`
+const Menu = styled.div`
+`
